472fed3edb4145dd8f1becc8e2f66210645d4fe7
Surya-V-28 <suryaarmy231@gmail.com>
2023-06-16 13:50:09 +0530

    init commit

---
+++ Assignment3/index.js
@@ -0,0 +1,8 @@
+
+
+function mobileResponse() {
+     var check  = document.getElementById("mobile-bar");
+     var res =   document.getElementById("welc");
+     res.style.flexdirection="column";
+     console.log("working");
+}
